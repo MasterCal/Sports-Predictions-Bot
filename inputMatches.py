918ed06eb709cd1dace81352b551569734c3e8f1
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 09:48:39 -0700

    Update inputMatches.py
    Define inputMatches method

--- inputMatches.py
+++ inputMatches.py
@@ -10,3 +10,12 @@ gameResults[] # int list containing the results of each matchup
 weeklyScore # how many matchups a player picked correctly each week
 totalScore # self-explanatory
 
+def inputMatches():
+  numMatches = int(input("Enter the number of matches for this week: "))
+  
+  for i in range(0, numMatches):
+    homeTeams[i] = input("Enter the home team for matchup " + str(i+1) + ": ")
+    roadTeams[i] = input("Enter the road team for matchup " + str(i+1) + ": ")
+    
+  print(homeTeams)
+  print(roadTeams)
