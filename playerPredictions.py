d8735d07d42caee272d250f394eaa7a814822a81
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 16:03:06 -0700

    Update playerPredictions.py
    Counter variable starts at 1, not 0

--- playerPredictions.py
+++ playerPredictions.py
@@ -1,22 +1,22 @@
 # player makes their predictions
 
 def playerPredictions():
-  i = 0 # counter variable
+  i = 1 # counter variable
   dataString = "Your Picks/n/n" # final message to be sent
 
   print("Make your selections, one by one, as the bot presents them. If you make a mistake, you can make changes later.")
 
-  while(i < numGames):
+  while(i <= numGames):
      print("Who do you think will win between ", homeTeam[i], " and ", roadTeam[i], "?")
 
      playerpicks[i] = input("Enter 1 for the first team and 2 for the second team: ")
 
      if (playerPicks[i] == 1):
        print("You predicted " + homeTeam[i])
-       dataString += str(i + 1) + ". " + homeTeam[i] + "/n"
+       dataString += str(i) + ". " + homeTeam[i] + "/n"
      elif (playerPicks[i] == 2):
        print("You predicted " + roadTeam[i])
-       dataString += str(i + 1) + ". " + roadTeam[i] + "/n"
+       dataString += str(i) + ". " + roadTeam[i] + "/n"
      else:
        while(playerPicks[i] != 1 or playerPicks[i] != 2):
           playerpicks[i] = input("Invalid input, please type 1 or 2")
