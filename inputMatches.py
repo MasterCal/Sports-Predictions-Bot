4ec5e84f9d3b1389d344649c66728ca77d2f2174
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 09:41:46 -0700

    Update inputMatches.py
    Declare global variables

--- inputMatches.py
+++ inputMatches.py
@@ -1,2 +1,12 @@
 # Admin inputs the matches performed each week
 
+# global variables
+
+numMatches # int number of NFL macthes in a given week
+homeTeams[] # str list containing the home teams for each matchup
+roadTeams[] # str list conntaining the road teams for each matchup
+playerPredictions[] # int list containing a player's picks for each matchup
+gameResults[] # int list containing the results of each matchup
+weeklyScore # how many matchups a player picked correctly each week
+totalScore # self-explanatory
+
