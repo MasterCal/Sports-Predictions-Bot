e7faf961acd5280796c93f5cfc1a3baa3c1e6816
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 16:20:22 -0700

    Update playerPredictions.py

--- playerPredictions.py
+++ playerPredictions.py
@@ -1,15 +1,15 @@
 # player makes their predictions
 
 def playerPredictions():
-  i = 1 # counter variable
+  i = 0 # counter variable
   dataString = "Your Picks/n/n" # final message to be sent
 
   print("Make your selections, one by one, as the bot presents them. If you make a mistake, you can make changes later.")
 
-  while(i <= numGames):
+  while(i < numGames):
      print("Who do you think will win between ", homeTeam[i], " and ", roadTeam[i], "?")
 
-     playerpicks[i] = input("Enter 1 for the first team and 2 for the second team: ")
+     playerpicks.append(input("Enter 1 for the first team and 2 for the second team: "))
 
      if (playerPicks[i] == 1):
        print("You predicted " + homeTeam[i])
