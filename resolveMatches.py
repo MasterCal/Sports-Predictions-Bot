d4d2ccaae464e86d316dae3d2068c0532c876e45
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 16:04:32 -0700

    Update resolveMatches.py
    Lists start at 1, not 0

--- resolveMatches.py
+++ resolveMatches.py
@@ -4,7 +4,7 @@ def resolvePredictions():
   dataString = "Results/n/n" #cresults of the matches
   leaderboard = "" # weekly and total leaderboards
   
-  for i in range(0, numMatches):
+  for i in range(1, numMatches+1):
     gameResults[i] = int(input("Who won between " + homeTeam[i] + " and " + roadTeam[i] + "? ")) # 1 for home, 2 for away, 0 for a tie
     
     if (gameResults[i] == 0): # match is a draw
