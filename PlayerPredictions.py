665bfe3ae3dcf422445d13fbbb7d9fd9f79c77fc
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 21:27:13 -0700

    Update PlayerPredictions.py

--- PlayerPredictions.py
+++ PlayerPredictions.py
@@ -1,30 +1,31 @@
 # player makes their predictions
+import GlobalVars
 
 def playerPredictions():
-  global numGames
-  global homeTeams
-  global roadTeams
-  global playerPicks
+  global GlobalVars.numGames
+  global GlobalVars.homeTeams
+  global GlobalVars.roadTeams
+  global GlobalVars.playerPicks
   
   i = 0 # counter variable
   dataString = "Your Picks/n/n" # final message to be sent
 
   print("Make your selections, one by one, as the bot presents them. If you make a mistake, you can make changes later.")
 
-  while(i < numGames):
-     print("Who do you think will win between ", homeTeams[i], " and ", roadTeams[i], "?")
+  while(i < GlobalVars.numGames):
+     print("Who do you think will win between ", GlobalVars.homeTeams[i], " and ", GlobalVars.roadTeams[i], "?")
 
-     playerpicks.append(input("Enter 1 for the first team and 2 for the second team: "))
+     GlobalVars.playerpicks.append(input("Enter 1 for the first team and 2 for the second team: "))
 
-     if (playerPicks[i] == 1):
-       print("You predicted " + homeTeams[i])
-       dataString += str(i) + ". " + homeTeams[i] + "/n"
-     elif (playerPicks[i] == 2):
-       print("You predicted " + roadTeams[i])
-       dataString += str(i) + ". " + roadTeams[i] + "/n"
+     if (GlobalVars.playerPicks[i] == 1):
+       print("You predicted " + GlobalVars.homeTeams[i])
+       dataString += str(i) + ". " + GlobalVars.homeTeams[i] + "/n"
+     elif (GlobalVars.playerPicks[i] == 2):
+       print("You predicted " + GlobalVars.roadTeams[i])
+       dataString += str(i) + ". " + GlobalVars.roadTeams[i] + "/n"
      else:
-       while(playerPicks[i] != 1 or playerPicks[i] != 2):
-          playerPicks[i] = input("Invalid input, please type 1 or 2")
+       while(GlobalVars.playerPicks[i] != 1 or GlobalVars.playerPicks[i] != 2):
+          GlobalVars.playerPicks[i] = input("Invalid input, please type 1 or 2")
    
      i += 1
     
