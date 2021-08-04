1ffcf235a79cb66222054e806846f198c9bc54db
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 21:29:51 -0700

    Update ResolveMatches.py

--- ResolveMatches.py
+++ ResolveMatches.py
@@ -1,39 +1,39 @@
 # admin inputs results of the matches, and scores are calculated
 
 def resolvePredictions():
-  global numMatches
-  global gameResults
-  global weeklyScore
-  global totalScore
-  global playerPicks
-  global homeTeams
-  global roadTeams
+  global GlobalVars.numMatches
+  global GlobalVars.gameResults
+  global GlobalVars.weeklyScore
+  global GlobalVars.totalScore
+  global GlobalVars.playerPicks
+  global GlobalVars.homeTeams
+  global GlobalVars.roadTeams
   
   dataString = "Results/n/n" #cresults of the matches
   leaderboard = "" # weekly and total leaderboards
   
-  for i in range(0, numMatches):
-    gameResults.append(int(input("Who won between " + homeTeams[i] + " and " + roadTeams[i] + "? "))) # 1 for home, 2 for away, 0 for a tie
+  for i in range(0, GlobalVars.numMatches):
+    GlobalVars.gameResults.append(int(input("Who won between " + GlobalVars.homeTeams[i] + " and " + GlobalVars.roadTeams[i] + "? "))) # 1 for home, 2 for away, 0 for a tie
     
-    if (gameResults[i] == 0): # match is a draw
-      weeklyScore += 0.5
-      totalScore += 0.5
-      dataString += homeTeams[i] + " and " + roadTeams[i] + " tied/n"
-    elif (gameResults[i] == playerPicks[i]): # correct prediction
-      weeklyScore += 1
-      totalScore += 1
+    if (GlobalVars.gameResults[i] == 0): # match is a draw
+      GlobalVars.weeklyScore += 0.5
+      GlobalVars.totalScore += 0.5
+      dataString += GlobalVars.homeTeams[i] + " and " + GlobalVars.roadTeams[i] + " tied/n"
+    elif (GlobalVars.gameResults[i] == GlobalVars.playerPicks[i]): # correct prediction
+      GlobalVars.weeklyScore += 1
+      GlobalVars.totalScore += 1
       
-    if (gameResults[i] == 1):
-      dataString += homeTeams[i] + " def. " + roadTeams[i] + "\n"
+    if (GlobalVars.gameResults[i] == 1):
+      dataString += GlobalVars.homeTeams[i] + " def. " + GlobalVars.roadTeams[i] + "\n"
     elif (gameResults[i] == 2):
-      dataString += roadTeams[i] + " def. " + homeTeams[i] + "\n"
+      dataString += GlobalVars.roadTeams[i] + " def. " + GlobalVars.homeTeams[i] + "\n"
       
   print(dataString)
   
   leaderboard += "Weekly Score/n/n" 
-  leaderboard += weeklyScore
+  leaderboard += GlobalVars.weeklyScore
   leaderboard += "/n/nTotal Score"
-  leaderboard += totalScore
+  leaderboard += GlobalVars.totalScore
   
   print(leaderboard)
     
