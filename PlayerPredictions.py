f39a3d28f23f3ce677a3fa6c2aa5c50e6cd1fe70
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 21:11:34 -0700

    Update PlayerPredictions.py
    Global variables, fixed list names

--- PlayerPredictions.py
+++ PlayerPredictions.py
@@ -1,25 +1,26 @@
 # player makes their predictions
 
 def playerPredictions():
+  global numGames, homeTeams, roadTeams, playerPicks
   i = 0 # counter variable
   dataString = "Your Picks/n/n" # final message to be sent
 
   print("Make your selections, one by one, as the bot presents them. If you make a mistake, you can make changes later.")
 
   while(i < numGames):
-     print("Who do you think will win between ", homeTeam[i], " and ", roadTeam[i], "?")
+     print("Who do you think will win between ", homeTeams[i], " and ", roadTeams[i], "?")
 
      playerpicks.append(input("Enter 1 for the first team and 2 for the second team: "))
 
      if (playerPicks[i] == 1):
-       print("You predicted " + homeTeam[i])
-       dataString += str(i) + ". " + homeTeam[i] + "/n"
+       print("You predicted " + homeTeams[i])
+       dataString += str(i) + ". " + homeTeams[i] + "/n"
      elif (playerPicks[i] == 2):
-       print("You predicted " + roadTeam[i])
-       dataString += str(i) + ". " + roadTeam[i] + "/n"
+       print("You predicted " + roadTeams[i])
+       dataString += str(i) + ". " + roadTeams[i] + "/n"
      else:
        while(playerPicks[i] != 1 or playerPicks[i] != 2):
-          playerpicks[i] = input("Invalid input, please type 1 or 2")
+          playerPicks[i] = input("Invalid input, please type 1 or 2")
    
      i += 1
     
