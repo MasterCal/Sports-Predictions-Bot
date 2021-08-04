7883b7426a140545de59325a5d8f2f8b08c229b1
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 21:13:27 -0700

    Update ResolveMatches.py
    Added global variables, fixed list names

--- ResolveMatches.py
+++ ResolveMatches.py
@@ -1,24 +1,26 @@
 # admin inputs results of the matches, and scores are calculated
 
 def resolvePredictions():
+  global numMatches, gameResults, weeklyScore, totalScore, playerPicks, homeTeams, roadTeams
+  
   dataString = "Results/n/n" #cresults of the matches
   leaderboard = "" # weekly and total leaderboards
   
   for i in range(0, numMatches):
-    gameResults.append(int(input("Who won between " + homeTeam[i] + " and " + roadTeam[i] + "? "))) # 1 for home, 2 for away, 0 for a tie
+    gameResults.append(int(input("Who won between " + homeTeams[i] + " and " + roadTeams[i] + "? "))) # 1 for home, 2 for away, 0 for a tie
     
     if (gameResults[i] == 0): # match is a draw
       weeklyScore += 0.5
       totalScore += 0.5
-      dataString += homeTeam[i] + " and " + roadTeam[i] + " tied/n"
+      dataString += homeTeams[i] + " and " + roadTeams[i] + " tied/n"
     elif (gameResults[i] == playerPicks[i]): # correct prediction
       weeklyScore += 1
       totalScore += 1
       
     if (gameResults[i] == 1):
-      dataString += homeTeam[i] + " def. " + roadTeam[i] + "\n"
+      dataString += homeTeams[i] + " def. " + roadTeams[i] + "\n"
     elif (gameResults[i] == 2):
-      dataString += roadTeam[i] + " def. " + homeTeam[i] + "\n"
+      dataString += roadTeams[i] + " def. " + homeTeams[i] + "\n"
       
   print(dataString)
   
