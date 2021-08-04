86f99132bed5869a8e606344ac6fb6477f94c1f8
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-03 21:23:52 -0700

    Create GlobalVars.py
    Contains all the global variables

---
+++ GlobalVars.py
@@ -0,0 +1,7 @@
+numMatches = 16 # int number of NFL matches in a given week
+homeTeams = [] # str list containing the home teams for each matchup
+roadTeams = [] # str list conntaining the road teams for each matchup
+playerPicks = [] # int list containing a player's picks for each matchup
+gameResults = [] # int list containing the results of each matchup
+weeklyScore = 0 # how many matchups a player picked correctly each week
+totalScore = 0 # self-explanatory
