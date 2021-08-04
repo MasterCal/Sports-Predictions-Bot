40483b1ae9d23762411076f816de45e5a70822f6
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-04 14:18:44 -0700

    Create EditPicks.py

---
+++ EditPicks.py
@@ -0,0 +1,3 @@
+# players can make changes to their picks, if they make a mistake, or they change their mind
+
+import datetime # don't allow players to retroactively edit after a game has already begun
