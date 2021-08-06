1f6421b98de7fd5ac52101463e4a005d08fe048f
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-06 15:36:13 -0700

    Update PlayerPredictions.py

--- PlayerPredictions.py
+++ PlayerPredictions.py
@@ -12,24 +12,23 @@ def playerPredictions():
     while True:
       try:
         GlobalVars.playerPicks[i] = int(GlobalVars.playerPicks[i])
+        if (GlobalVars.playerPicks[i] != 1 and GlobalVars.playerPicks[i] != 2):
+          raise Exception
       except ValueError:
         GlobalVars.playerPicks[i] = input("Invalid value input, please type 1 or 2: ")
-      else:
-        while (GlobalVars.playerPicks[i] != 1 and GlobalVars.playerPicks[i] != 2)
+      except Exception:
+        while (GlobalVars.playerPicks[i] != 1 and GlobalVars.playerPicks[i] != 2):
           GlobalVars.playerPicks[i] = input("Invalid int input, please type 1 or 2")
+      else:
+        if (GlobalVars.playerPicks[i] == 1): # print the player's pick
+          print("You predicted " + GlobalVars.homeTeams[i])
+          dataString += str(i+1) + ". " + GlobalVars.homeTeams[i] + "\n"
+        elif (GlobalVars.playerPicks[i] == 2):
+          print("You predicted " + GlobalVars.roadTeams[i])
+          dataString += str(i+1) + ". " + GlobalVars.roadTeams[i] + "\n"
         break
         
-    if (GlobalVars.playerPicks[i] == 1): # print the player's pick
-      print("You predicted " + GlobalVars.homeTeams[i])
-      dataString += str(i+1) + ". " + GlobalVars.homeTeams[i] + "\n"
-    elif (GlobalVars.playerPicks[i] == 2):
-      print("You predicted " + GlobalVars.roadTeams[i])
-      dataString += str(i+1) + ". " + GlobalVars.roadTeams[i] + "\n"
-     
     i += 1
      
   print(dataString)
   print("If you wish to make any changes, use picks edit")
-
- 
-    
