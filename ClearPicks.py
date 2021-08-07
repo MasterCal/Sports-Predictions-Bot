9b81f9d2219448b89de8da718528a431870cd141
MasterCal <88394390+MasterCal@users.noreply.github.com>
2021-08-07 13:46:05 -0700

    Update ClearPicks.py

--- ClearPicks.py
+++ ClearPicks.py
@@ -1,3 +1,10 @@
 # reset weekly picks so admin can enter the next week's matchups
 
 import GlobalVars
+
+def clear():
+    GlobalVars.homeTeams = []
+    GlobalVars.roadTeams = []
+    GlobalVars.playerPicks = []
+    GlobalVars.gameResults = []
+    GlobalVars.weeklyScore = 0
